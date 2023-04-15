df9e0e4dd8e4cc64e3ac40729738b0b0371dbb3b
Matias Sauvanet <matiassauvanet@gmail.com>
2023-04-15 18:10:27 -0300

    BaSP-M2023: Add exercises and vas answer

---
+++ Week-05/exercises/variables-and-operators.js
@@ -0,0 +1,17 @@
+var a = 1;
+var b = 2;
+var c = a + b;
+
+console.log (c);
+
+var name = "Matias";
+var secondName = "Nahuel";
+var completeName = name + '' + secondName;
+
+console.log (completeName);
+
+var o = "tank";
+var d = "infantery";
+var od = o + d;
+
+console.log (od.length)
